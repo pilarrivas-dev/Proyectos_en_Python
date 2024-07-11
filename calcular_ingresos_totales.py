f20b89748944c6c6506d83006cc50128c13d67fa
Pilar <pilarrivas.dev@gmail.com>
2024-07-11 14:44:54 +0200

    Se muestra los ingresos_totales

--- calcular_ingresos_totales.py
+++ calcular_ingresos_totales.py
@@ -4,3 +4,7 @@ s2=int(input("Introduce el segundo ingreso:"))
 def sumar(s1,s2):
     resultado = s1 +s2
     return resultado
+
+ingresos_totales = sumar(s1,s2)
+
+print("Los ingresos totales son:", ingresos_totales)
