8ae0975bae51240c5d7772a0202ea5eed7fae9fa
Pilar <pilarrivas.dev@gmail.com>
2024-07-11 14:23:04 +0200

    Se piden los numeros que se van a utilizar en las operaciones.

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,3 @@
 num= int(input("¿Que operacion quieres realizar: 1.Suma, 2.Resta, 3. Multiplicacion y 4.Division?"))
+numero1=int(input("Introduce el primer numero:"))
+numero2=int(input("Introduce el segundo numero:"))
